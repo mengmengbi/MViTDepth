d872905b08dd2486ad1cb5fb7c08e2a5fe048d78
mengmengbi <48798223+mengmengbi@users.noreply.github.com>
2023-06-22 17:03:04 +0800

    Update test_simple.py

--- test_simple.py
+++ test_simple.py
@@ -148,8 +148,7 @@ def test_simple(args):
             colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
             im = pil.fromarray(colormapped_im)
 
-            name_dest_im = os.path.join(r"F:\rd\mobilevit_distilla_pose_nofreeze_light_decoder\assets\result\s_1_4_640",
-                                        "{}_disp.jpeg".format(output_name))
+            name_dest_im = os.path.join(output_name)
             im.save(name_dest_im)
 
             print("   Processed {:d} of {:d} images - saved predictions to:".format(
